42c2ba0259064c97d3802453c3d86704bed77594
Maria Alvarez <maria.alvarez9296@gmail.com>
2024-04-23 22:38:26 +0200

    fix login

--- src/main/java/com/abl/controller/UserController.java
+++ src/main/java/com/abl/controller/UserController.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abl.entity.User;
@@ -24,23 +21,14 @@ public class UserController {
 
 	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/new")
-	public ResponseEntity<String> createUser(String dni, String name, String email, int tlf, String pass) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		//Comprueba si existe el dni
-		if (usuarioRepository.existsById(dni)) {
+		if (usuarioRepository.existsById(user.getDni())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User with this DNI already exists");
-        } 
-		
-		User newUser = new User();
-		newUser.setDni(dni);
-		newUser.setName(name);
-		newUser.setEmail(email);
-		newUser.setTlf(tlf);
-		newUser.setPassword(pass);
-
+        }
 		// Guardar el nuevo usuario en la base de datos
-		usuarioRepository.save(newUser);
-
-		return ResponseEntity.ok("User created successfully");		
+		usuarioRepository.save(user);
+		return ResponseEntity.ok().body(user);		
 	}
 	
 	@CrossOrigin(origins = "http://localhost:4200")
@@ -51,7 +39,6 @@ public class UserController {
         if (existingUser == null || !existingUser.getPassword().equals(user.getPassword())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
-
         // Usuario autenticado correctamente
         return ResponseEntity.ok().body(existingUser);
     }
