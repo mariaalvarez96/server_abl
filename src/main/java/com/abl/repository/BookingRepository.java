e4ddfad2f6a869b34ed7d0b20f35a94a86885b6a
mariaalvarez96 <maria.alvarez9296@gmail.com>
2024-04-25 13:49:39 +0200

    booking model + repository + controller

---
+++ src/main/java/com/abl/repository/BookingRepository.java
@@ -0,0 +1,11 @@
+package com.abl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abl.entity.Booking;
+
+public interface BookingRepository extends JpaRepository<Booking, String>{
+
+	@SuppressWarnings("unchecked")
+	Booking save(Booking booking);
+}
